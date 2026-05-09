47470464e436d9cb2a6339f1e938739056607522
Kutlucan571 <kutlucan0571@gmail.com>
2026-05-09 18:18:21 +0300

    supabase url hatasi duzeltildi

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Bu bilgileri yukarıda bulduğun bilgilerle değiştir
-const supabaseUrl = 'https://mqbtzsdizqddggljmqzl.supabase.co/rest/v1/'
+const supabaseUrl = 'https://mqbtzsdizqddggljmqzl.supabase.co'
 const supabaseKey = 'sb_publishable_IJI3l0lWaFV2WIuFGzaiHA_6rsGwcJb'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
